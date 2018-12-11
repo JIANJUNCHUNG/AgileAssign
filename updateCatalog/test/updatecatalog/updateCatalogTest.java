38e09e1628e605a01bcd10d95082b9dc96b8cb73
Student <Student@PC-75>
2018-12-11 09:35:29 +0800

    update catalog junit test

---
+++ updateCatalog/test/updatecatalog/updateCatalogTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package updatecatalog;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class updateCatalogTest {
+    
+    public updateCatalogTest() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testProductList() {
+    }
+
+    @Test
+    public void testAddRowtoTable() {
+    }
+
+    @Test
+    public void testMain() {
+    }
+    
+}
